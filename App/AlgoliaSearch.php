ff2ffbb605f1e7ff2a395c8cf4b8bb4ac9b530e7
Julie Engel <jue@metanet.ch>
2016-05-02 16:06:41 +0200

    make AlgoliaSearch adhere to SearchInterface

--- App/AlgoliaSearch.php
+++ App/AlgoliaSearch.php
@@ -10,10 +10,29 @@ use App\Interfaces\searchInterface;
  */
 class AlgoliaSearch implements SearchInterface
 {
+    protected $client;
+    protected $index;
+
     public function __construct()
     {
-        return new \AlgoliaSearch\Client("3VC8H0ZENS", "a156adee94868ae9d981ae9f1df2ff49");
+        $this->client = new \AlgoliaSearch\Client("3VC8H0ZENS", "a156adee94868ae9d981ae9f1df2ff49");
     }
+
+    public function index($searchIndex)
+    {
+        $this->index = $this->client->initIndex($searchIndex);
+        return $this;
+    }
+
+    public function find($searchQuery, $searchOptions)
+    {
+        if ($this->index) {
+            return $this->index->search($searchQuery, $searchOptions)['hits'];
+        }
+
+        return null;
+    }
+    
 }
 
 //$client = new \AlgoliaSearch\Client("3VC8H0ZENS", "a156adee94868ae9d981ae9f1df2ff49");
